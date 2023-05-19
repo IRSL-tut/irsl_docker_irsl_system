9a2233b89412c6cd693c54ddc404023f27f217e1
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2023-05-19 19:28:14 +0900

    add devel

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 
 ## noetic or melodic
 ROS_DISTRO_=${BUILD_ROS:-"noetic"}
-UBUNTU_VER="22.04"
+UBUNTU_VER="22.04" ## humble
 if [ ${ROS_DISTRO_} == "noetic" ]; then
     UBUNTU_VER="20.04"
 elif [ ${ROS_DISTRO_} == "melodic" ]; then
@@ -24,8 +24,13 @@ if [ -n ${NO_CACHE} ]; then
     DOCKER_OPT="--no-cache ${DOCKER_OPT}"
 fi
 
+DOCKER_FILE=Dockerfile.build_system
+if [ -n ${BUILD_DEVEL} ]; then
+    DOCKER_FILE=Dockerfile.build_system.devel
+fi
+
 echo "Build Image: ${TARGET_IMG}"
 
-docker build . ${DOCKER_OPT} -f Dockerfile.add_xeus     --build-arg BASE_IMAGE=${BASE_IMG} --build-arg BUILD_IMAGE=${XEUS_IMG} -t build_temp/build_system:0
+docker build . ${DOCKER_OPT} -f Dockerfile.add_xeus  --build-arg BASE_IMAGE=${BASE_IMG} --build-arg BUILD_IMAGE=${XEUS_IMG} -t build_temp/build_system:0
 
-docker build . ${DOCKER_OPT} -f Dockerfile.build_system --build-arg BASE_IMAGE=build_temp/build_system:0 --build-arg UBUNTU_VER=${UBUNTU_VER} -t ${TARGET_IMG}
+docker build . ${DOCKER_OPT} -f ${DOCKER_FILE}       --build-arg BASE_IMAGE=build_temp/build_system:0 --build-arg UBUNTU_VER=${UBUNTU_VER} -t ${TARGET_IMG}
