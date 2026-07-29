e32812d99676efe1380e7c9746d05185c51b9017
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2026-07-29 21:21:12 +0900

    update make_ws.sh

--- make_ws.sh
+++ make_ws.sh
@@ -10,19 +10,23 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 ### install choreonoid
 if [ "${ROS_DISTRO}" == "noetic" -o "${ROS_DISTRO}" == "one" ]; then
     wget https://raw.githubusercontent.com/IRSL-tut/irsl_choreonoid/main/config/dot.rosinstall  -O dot.rosinstall
-    CNOID_ROS='stable';
+    CNOID_ROS_VER='stable';
+    CNOID_ROS_NAME='choreonoid_ros';
+    CNOID_ROS_REPO='https://github.com/IRSL-tut/choreonoid_ros.git';
     IRSL_CNOID_ROS='main';
 else
     wget https://raw.githubusercontent.com/IRSL-tut/irsl_choreonoid/main/config/dot.ros2install -O dot.rosinstall
-    CNOID_ROS='devel_20260312';
+    CNOID_ROS_VER='devel';
+    CNOID_ROS_NAME='choreonoid_ros2';
+    CNOID_ROS_REPO='https://github.com/IRSL-tut/choreonoid_ros2.git';
     IRSL_CNOID_ROS='devel_ros2_24.04';
 fi
 cat <<- _DOC_ >> dot.rosinstall
 ### IRSL settings >>> ###
 - git:
-    local-name: choreonoid_ros
-    uri: https://github.com/IRSL-tut/choreonoid_ros.git
-    version: ${CNOID_ROS}
+    local-name: ${CNOID_ROS_NAME}
+    uri: ${CNOID_ROS_REPO}
+    version: ${CNOID_ROS_VER}
 - git:
     local-name: irsl_choreonoid_ros
     uri: https://github.com/IRSL-tut/irsl_choreonoid_ros.git
@@ -47,13 +51,17 @@ _DOC_
 
 source /opt/ros/${ROS_DISTRO}/setup.bash && \
     (mkdir src; cd src; vcs import --recursive < ../dot.rosinstall) && \
-    patch -d src -p1 < src/irsl_choreonoid/config/choreonoid_closed_ik.patch && \
     sed -i -e "s@osqp-cpp src/osqp++.cc@osqp-cpp SHARED src/osqp++.cc@g" src/qp_solvers/osqp-cpp/osqp-cpp/CMakeLists.txt && \
     find src/prioritized_qp src/ik_solvers src/qp_solvers \
          -name CMakeLists.txt -exec sed -i -e s@-std=c++[0-9][0-9]@-std=c++17@g {} \;
+##    patch -d src -p1 < src/irsl_choreonoid/config/choreonoid_closed_ik.patch && \
 
 ### add cgal to workspace for cnoid_cgal
-(cd src; mkdir -p cgal; wget https://github.com/CGAL/cgal/releases/download/v5.6.2/CGAL-5.6.2.tar.xz -O - | tar Jxf - --strip-components 1 -C cgal)
+if $(echo -e "$(lsb_release -s -r)\n20.04" | sort -C -V); then \
+    (cd src; mkdir cgal; wget -q https://github.com/CGAL/cgal/releases/download/v5.6.2/CGAL-5.6.2.tar.xz -O - | tar Jxf - --strip-components 1 -C cgal)
+else
+    (cd src; mkdir cgal; wget -q https://github.com/CGAL/cgal/releases/download/v6.2/CGAL-6.2.tar.xz -O - | tar Jxf - --strip-components 1 -C cgal)
+fi
 #COPY files/cgal_package.xml src/cgal/package.xml
 cp ${SCRIPT_DIR}/files/cgal_package.xml src/cgal/package.xml
 
@@ -67,17 +75,22 @@ cp ${SCRIPT_DIR}/files/cgal_package.xml src/cgal/package.xml
 (cd src/choreonoid/ext; git clone https://github.com/IRSL-tut/irsl_cnoid_plugin.git)
 
 sudo apt update -q -qq && \
-    src/choreonoid/misc/script/install-requisites-ubuntu-$(lsb_release -s -r).sh && \
+    if $(echo -e "23.10\n$(lsb_release -s -r)" | sort -C -V); then \
+        UBUNTU_VER=24.04; \
+    else \
+        UBUNTU_VER=$(lsb_release -s -r); \
+    fi && \
+    src/choreonoid/misc/script/install-requisites-ubuntu-${UBUNTU_VER}.sh && \
     if [ "$ROS_DISTRO" = "noetic" -o "$ROS_DISTRO" = "one" ]; then \
         sudo apt install -q -qq -y python3-catkin-tools libreadline-dev ; \
-    elif [ "$ROS_DISTRO" = "jazzy" -o "$ROS_DISTRO" = "humble" -o "$ROS_DISTRO" = "rolling" ]; then \
+    elif [ "$ROS_DISTRO" = "lyrical" -o  "$ROS_DISTRO" = "jazzy" -o "$ROS_DISTRO" = "humble" -o "$ROS_DISTRO" = "rolling" ]; then \
         sudo apt install -q -qq -y libreadline-dev ; \
     else \
         sudo apt install -q -qq -y python-catkin-tools libreadline-dev ; \
     fi && \
     sudo apt install -q -qq -y libpulse-dev libsndfile-dev gstreamer1.0-libav libgstreamer-plugins-base1.0-dev libgstreamer1.0-dev && \
     rosdep update -y -q -r && \
-    rosdep install -y -q -r --ignore-src --from-path src/choreonoid_ros src/irsl_choreonoid_ros src/cnoid_cgal
+    rosdep install -y -q -r --ignore-src --from-path src/choreonoid_ros* src/irsl_choreonoid_ros src/cnoid_cgal
 
 if [ "${ROS_DISTRO}" == "noetic" -o "${ROS_DISTRO}" == "one" ]; then
     ## build using catkin
@@ -86,5 +99,5 @@ if [ "${ROS_DISTRO}" == "noetic" -o "${ROS_DISTRO}" == "one" ]; then
 else
     ## build using ament
     echo 'run command below';
-    echo '/bin/bash -c "source /opt/ros/${ROS_DISTRO}/setup.bash && colcon build --parallel-workers 1 --merge-install --event-handlers console_direct+ desktop_notification- log_command+ status- --packages-up-to irsl_choreonoid_ros"';
+    echo '/bin/bash -c "source /opt/ros/${ROS_DISTRO}/setup.bash && colcon build --parallel-workers 1 --merge-install --event-handlers console_direct+ desktop_notification- log_command+ status- --cmake-args -DCMAKE_POLICY_VERSION_MINIMUM=3.5 --ament-cmake-args -DCMAKE_POLICY_VERSION_MINIMUM=3.5 --packages-up-to irsl_choreonoid_ros"';
 fi
